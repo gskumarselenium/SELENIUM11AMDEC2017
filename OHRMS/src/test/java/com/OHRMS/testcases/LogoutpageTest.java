559cc073ee2b9868f269e8e82f201cda4b37ecb3
sudheer <gskumarselenium@gmail.com>
2017-12-13 13:02:09 +0530

    logout testcase added to the testcases

---
+++ OHRMS/src/test/java/com/OHRMS/testcases/LogoutpageTest.java
@@ -0,0 +1,14 @@
+package com.OHRMS.testcases;
+
+import java.io.IOException;
+
+import com.OHRMS.baseclass.Baseclass;
+
+public class LogoutpageTest extends Baseclass  {
+
+	public LogoutpageTest() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
